d78e19d3c5d743fb1c7a2bccc2380d1bc49889b4
ethan <david19870709@hotmail.com>
2021-03-04 12:03:03 +0800

    fit for Rorze gem300

---
+++ RorzeSECS/secs4net/Core/ConnectionState.cs
@@ -0,0 +1,11 @@
+//namespace Secs4Net
+namespace Rorze.SECS.Driver
+{
+    public enum ConnectionState
+    {
+        Connecting,
+        Connected,
+        Selected,
+        Retry
+    }
+}
---
+++ RorzeSECS/secs4net/Core/SecsException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+//namespace Secs4Net
+namespace Rorze.SECS.Driver
+{
+    public sealed class SecsException : Exception
+    {
+        public SecsMessage SecsMsg { get; }
+
+        public SecsException(SecsMessage msg, string description)
+            : base(description)
+        {
+            SecsMsg = msg;
+        }
+
+        public SecsException(string msg)
+            : this(null, msg)
+        {
+        }
+    }
+}
